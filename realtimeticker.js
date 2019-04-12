9b468fb128ed70a275076826eaabafaeb157d9e5
markblack96 <markblack96@gmail.com>
2019-04-12 00:08:39 -0500

    Updates

--- realtimeticker.js
+++ realtimeticker.js
@@ -1,13 +1,23 @@
 var speed = 16;
-var width = 240;
-var height = 480;
 
+
+var margin = {top: 32, right: 16, bottom: 32, left: 16},
+    width = 240 - margin.left - margin.right,
+    height = 480 - margin.top - margin.bottom;
+    
 var svg = d3.select('#svg-holder').append('svg')
-            .attr("width", width)
-            .attr("height", height);
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 
 var y = d3.scaleLinear().domain([0, 32]).range([height, 0]);
+
+var y_axis = d3.axisRight().scale(y)
+                .ticks(20, "s");
+
+// var gY = svg.append("g").attr("transform", "translate(0, height)").call(y_axis);
 
 draw();
 
@@ -28,20 +38,33 @@ function draw() {
         .attr('x', 64)
         .attr('y', 32)
         .text(function(d) { return "Speed: " + d + "km/h"; });
+        
+    var gY = svg.append("g").call(y_axis);
+    
+    /*
+    var location_text = svg.selectAll('#location_text')
+                            .data([location])
+                            .enter()
+                            .append('text')
+                            .attr('x', 64)
+                            .attr('y', 48)
+                            .attr('id', 'location_text')
+                            .text( function(d) { return "GPS location: " + location.latitude; });
+    */
 }
 
 function redraw(datum) {
     var bar = svg.selectAll('rect').data([datum]);
 
     bar.transition()
-        .duration(1000)
+        .duration(950)
         .attr("y", function(d) { return y(d); })
         .attr("height", function(d) { return height - y(d);});
         
     var text = svg.selectAll('text').data([datum]).transition()
+        .duration(50)
 //        .enter().append('text')
         .text(function(d) { return "Speed: " + d + "km/h"; } );
-        
 }
 
 
@@ -50,7 +73,11 @@ function redraw(datum) {
 d3.interval(function(){   
             // add or subtract based on value of Math.random()
             // if < 0.5 then -1, else 1
-            speed += Math.random() < 0.5 ? -1 : 1;
+            ds = Math.random() < 0.5 ? -1 : 1;
+            // if speed>0 and <32 add ds to speed, else add opposite of ds
+            speed += speed > 0 & speed < 32 ? ds : ds * -1;
+            
+            
             console.log(speed);
             redraw(speed);
          }, 1000);
